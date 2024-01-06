78279d14bca7333ba92fa2161b0aae1ac7fa347b
mackwick <mackenziewicker@gmail.com>
2024-01-06 10:42:17 -0500

    finished auth

--- controllers/fruitrouter.js
+++ controllers/fruitrouter.js
@@ -1,108 +1,109 @@
 //DEPENDENCIES
-const express = require("express")
-const Fruit = require("../models/Fruit")
+const express = require("express");
+const Fruit = require("../models/Fruit");
 
 //CREATE ROUTER
-const router = express.Router()
+const router = express.Router();
 
 //MIDDLEWARE
 router.use((req, res, next) => {
-    console.table(req.session);
-  
-    if (req.session.loggedIn) {
-      next();
-    } else {
-      res.redirect("/user/login");
-    }
-  
-    
-  });
+  console.table(req.session);
+
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect("/user/login");
+  }
+});
 
 //ROUTES
 
 //Index route
 router.get("/", async (req, res) => {
-    try{
-        const fruits = await Fruit.find({})
-        res.render("fruits/index.ejs", {fruits})
-    } catch(error){
-        console.log("------------", error.message)
-        res.status(400).send("error, read logs for details")
-    }
-})
+  try {
+    //get username from sesh
+    const username = req.session.username;
+    const fruits = await Fruit.find({ username });
+    res.render("fruits/index.ejs", { fruits });
+  } catch (error) {
+    console.log("------------", error.message);
+    res.status(400).send("error, read logs for details");
+  }
+});
 
 //New route
 router.get("/new", (req, res) => {
-    res.render("fruits/new.ejs")
-})
+  res.render("fruits/new.ejs");
+});
 
 //Destroy route
 router.delete("/:id", async (req, res) => {
-    try{
-        const id = req.params.id
-        await Fruit.findByIdAndDelete(id)
-        res.redirect("/fruits")
-    } catch(error) {
-        console.log("------------", error.message)
-        res.status(400).send("error, read logs for details")
-    }
-})
+  try {
+    const id = req.params.id;
+    await Fruit.findByIdAndDelete(id);
+    res.redirect("/fruits");
+  } catch (error) {
+    console.log("------------", error.message);
+    res.status(400).send("error, read logs for details");
+  }
+});
 
 //Update route
-router.put("/:id", async (req, res) =>{
-    try{
-        const id = req.params.id
-        //update ready to eat
-        req.body.readyToEat = req.body.readyToEat === "on" ? true : false
-        //update the fruit
-        await Fruit.findByIdAndUpdate(id, req.body)
-        res.redirect(`/fruits/${id}`)
-    }catch(error) {
-        console.log("------------", error.message)
-        res.status(400).send("error, read logs for details")
-    }
-})
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    //update ready to eat
+    req.body.readyToEat = req.body.readyToEat === "on" ? true : false;
+    //update the fruit
+    await Fruit.findByIdAndUpdate(id, req.body);
+    res.redirect(`/fruits/${id}`);
+  } catch (error) {
+    console.log("------------", error.message);
+    res.status(400).send("error, read logs for details");
+  }
+});
 
 //Create route
 router.post("/", async (req, res) => {
-    try{
-        //check if readyToEat should be true
-        req.body.readyToEat = req.body.readyToEat === "on" ? true : false
-        //create fruit
-        await Fruit.create(req.body)
-        //redirect to main 
-        res.redirect("/fruits")
-    }catch(error) {
-        console.log("------------", error.message)
-        res.status(400).send("error, read logs for details")
-    }
-})
+  try {
+    //check if readyToEat should be true
+    req.body.readyToEat = req.body.readyToEat === "on" ? true : false;
+    //add user to req.body from req.session
+    req.body.username = req.session.username;
+    //create fruit
+    await Fruit.create(req.body);
+    //redirect to main
+    res.redirect("/fruits");
+  } catch (error) {
+    console.log("------------", error.message);
+    res.status(400).send("error, read logs for details");
+  }
+});
 
 //Edit route
 router.get("/:id/edit", async (req, res) => {
-    try{
-        const id = req.params.id
-        const fruit = await Fruit.findById(id)
-        await res.render("fruits/edit.ejs", {fruit})
-    } catch(error) {
-        console.log("------------", error.message)
-        res.status(400).send("error, read logs for details")
-    }
-})
+  try {
+    const id = req.params.id;
+    const fruit = await Fruit.findById(id);
+    await res.render("fruits/edit.ejs", { fruit });
+  } catch (error) {
+    console.log("------------", error.message);
+    res.status(400).send("error, read logs for details");
+  }
+});
 
 //Show route
 router.get("/:id", async (req, res) => {
-    try{
-        const id = req.params.id
-        const fruit = await Fruit.findById(id)
-        //render template
-        res.render("fruits/show.ejs", {fruit})
-    }catch(error) {
-        console.log("------------", error.message)
-        res.status(400).send("error, read logs for details")
-    }
-})
-
+  try {
+    const id = req.params.id;
+    const fruit = await Fruit.findById(id);
+    //render template
+    res.render("fruits/show.ejs", { fruit });
+  } catch (error) {
+    console.log("------------", error.message);
+    res.status(400).send("error, read logs for details");
+  }
+});
 
 //EXPORT ROUTER
-module.exports = router
+module.exports = router;
--- models/Fruit.js
+++ models/Fruit.js
@@ -1,19 +1,20 @@
 //DEPENDENCIES
-const mongoose = require("./connection.js")
+const mongoose = require("./connection.js");
 
 //DEFINE FRUIT MODEL
 //destructure Schema and model into their own variables
-const {Schema, model} = mongoose
+const { Schema, model } = mongoose;
 
 //Schema - shape
 const fruitSchema = new Schema({
-    name: String,
-    color: String,
-    readyToEat: Boolean
-})
+  name: String,
+  color: String,
+  readyToEat: Boolean,
+  username: String,
+});
 
 //Model - object for interacting with the db
-const Fruit = model("Fruit", fruitSchema)
+const Fruit = model("Fruit", fruitSchema);
 
 //EXPORT
-module.exports = Fruit
+module.exports = Fruit;
